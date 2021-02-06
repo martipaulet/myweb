81e526d8d94ac73ecbfd829a9ff8fcbe85765d96
martipaulet <78548539+martipaulet@users.noreply.github.com>
2021-02-06 17:51:39 +0100

    javascript upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if(this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+
+        } else {
+            $('.navbar').removeClass("sticky");
+
+        }
+    });
+
+    //menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.home .home-text-content').toggleClass("none")
+        $('.menu-btn i').toggleClass("active");
+
+    })
+});
