20ac905b2ed6f42f470023fab01b8324373cef5c
martipaulet <78548539+martipaulet@users.noreply.github.com>
2021-02-08 14:21:39 +0100

    upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-$(document).ready(function() {
-    $(window).scroll(function(){
-        if(this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
+$(window).on("load", function () {
+    $(document).ready(function() {
+        $(window).scroll(function(){
+            if(this.scrollY > 20) {
+                $('.navbar').addClass("sticky");
 
-        } else {
-            $('.navbar').removeClass("sticky");
+            } else {
+                $('.navbar').removeClass("sticky");
 
-        }
-    });
+            }
+        });
 
-    //menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('body').toggleClass("no-scroll")
-        $('.home .home-text-content').toggleClass("none")
-        $('.menu-btn i').toggleClass("active");
+        //menu/navbar script
+        $('.menu-btn').click(function(){
+            $('.navbar .menu').toggleClass("active");
+            $('body').toggleClass("no-scroll")
+            $('.home .home-text-content').toggleClass("none")
+            $('.menu-btn i').toggleClass("active");
 
+        })
     })
 });
